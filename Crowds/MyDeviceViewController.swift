fe3fd7daac9a7fc7d8c5e5e01e425fb55f66e0aa
Jiahua Chen <jc@jh.tc>
2021-01-23 16:30:14 -0500

    Added tabs

---
+++ Crowds/MyDeviceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyDeviceViewController.swift
+//  Crowds
+//
+//  Created by Jiahua Chen on 1/23/21.
+//
+
+import UIKit
+
+class MyDeviceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
